f1a6c58976fa3ee9459745d1886bc8076fe100de
Kushma Thapa <>
2021-12-14 10:32:06 +0545

    KT: Add Fetch api with headers

--- pages/guestList/index.vue
+++ pages/guestList/index.vue
@@ -30,9 +30,31 @@ export default {
     }
   },
   async fetch() {
-    this.mountains = await fetch('https://api.netlify.com/api/v1/forms/guestEntry/submissions').then((res) =>
-      res.json()
-    )
+    this.mountains = await fetch(
+      'https://api.netlify.com/api/v1/sites/e9b463c3-3389-4787-9d0e-a57b34fd10f4/forms/61b77c04c4895f0007bf9fcd/submissions?state=ham&page=1&per_page=20',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-language': 'en-US,en;q=0.9',
+          authorization: 'Bearer tO_M-Vp-V7tbtwxEnHXH4vOTHozUAaebSrxm90PtfrQ',
+          'content-type': 'application/json',
+          'if-none-match': 'W/"1ce6bb9be42b0b771e03365209be3f26"',
+          'sec-ch-ua': '"Google Chrome";v="95", "Chromium";v="95", ";Not A Brand";v="99"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"macOS"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+        referrer: 'https://app.netlify.com/sites/eventguest/forms/61b77c04c4895f0007bf9fcd',
+        referrerPolicy: 'no-referrer-when-downgrade',
+        body: null,
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'include',
+      }
+    ).then((res) => res.json())
   },
 }
 </script>
+
